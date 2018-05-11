3bc58cfec2b84569dabda3669a144262ecb3fb6e
suruiliang <suruiliang_1988@163.com>
2018-05-11 18:17:10 +0800

    511

--- bec-cloud-auth-core/src/main/java/com/bec/cloud/auth/core/support/Constant.java
+++ bec-cloud-auth-core/src/main/java/com/bec/cloud/auth/core/support/Constant.java
@@ -1,12 +1,36 @@
 package com.bec.cloud.auth.core.support;
 
 public class Constant {
+	/**
+	 * 是否删除
+	 */
 	public final static class DelStatus{
 		public final static int NORMAL=0;
 		public final static int DELETED=1;
 	}
+	/**
+	 * 是否启用
+	 */
 	public final static class OnOffStatus{
 		public final static int ON=0;
 		public final static int OFF=1;
+	}
+	/**
+	 * 是否完成
+	 */
+	public final static class FinishStatus{
+		public final static int UNFINISH=0;
+		public final static int FINISHED=1;
+	}
+	/**
+	 * 审核状态
+	 */
+	public final static class TaskStatus{
+		public final static int UN_AUDIT=0;
+		public final static int AUDIT_ACCEPT=1;
+		public final static int AUDIT_REJECTED=2;
+		public final static int UN_FUYOU_AUDIT=3;
+		public final static int FUYOU_AUDIT_ACCEPT=4;
+		public final static int FUYOU_AUDIT_REJECTED=5;
 	}
 }
